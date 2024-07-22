a81c47f3bb95737964180423373b49707c21c109
pace11 <ryanjoker87@gmail.com>
2024-07-23 00:49:46 +0700

    feat: update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const { version } = require('./package.json')
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    const cf = config
+    cf.resolve.fallback = { fs: false }
+    return cf
+  },
   env: {
     NEXT_PUBLIC_APP_VERSION: version,
   },
